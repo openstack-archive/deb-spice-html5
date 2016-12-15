f29dcf43500d7c753ab1c263066478b66d34edcf
Jeremy White <jwhite@codeweavers.com>
2016-12-15 10:41:54 -0600

    Display stream creation and destruction at debug level 1.
    They are low frequency, useful messages and should be seen
    more readily.

--- display.js
+++ display.js
@@ -535,7 +535,7 @@ SpiceDisplayConn.prototype.process_channel_message = function(msg)
     if (msg.type == SPICE_MSG_DISPLAY_STREAM_CREATE)
     {
         var m = new SpiceMsgDisplayStreamCreate(msg.data);
-        STREAM_DEBUG > 1 && console.log(this.type + ": MsgStreamCreate id" + m.id +
+        STREAM_DEBUG > 0 && console.log(this.type + ": MsgStreamCreate id" + m.id +
                                         "; width " + m.stream_width + "; height " + m.stream_height +
                                         "; left " + m.dest.left + "; top " + m.dest.top
                                         );
@@ -639,7 +639,7 @@ SpiceDisplayConn.prototype.process_channel_message = function(msg)
     if (msg.type == SPICE_MSG_DISPLAY_STREAM_DESTROY)
     {
         var m = new SpiceMsgDisplayStreamDestroy(msg.data);
-        STREAM_DEBUG > 1 && console.log(this.type + ": MsgStreamDestroy id" + m.id);
+        STREAM_DEBUG > 0 && console.log(this.type + ": MsgStreamDestroy id" + m.id);
 
         if (this.streams[m.id].codec_type == SPICE_VIDEO_CODEC_TYPE_VP8)
         {
